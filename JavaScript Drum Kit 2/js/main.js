fa7e1e2ff4a29284c800820252a533af7e0b6b49
Shahfazlollahi <n_shahfazlollahi@tss-hr420-dm38.local>
2019-03-04 16:11:09 -0500

    added animtions

--- JavaScript Drum Kit 2/js/main.js
+++ JavaScript Drum Kit 2/js/main.js
@@ -9,13 +9,32 @@
 
 		//debugging / error handling
 		//if we dont have matching audio element, kill function
+		// ! is a "not" operator or a bang
 		if (!audio) { return; } //return  stops code execution
 
 		//rewind audio on every click and make it play
 		audio.currentTime = 0
 		audio.play();
 
+		//grab div and animate it
+		let key = document.querySelector(`div[data-key="${event.keyCode}"]`)
+		key.classList.add('playing');
 	}
+
+	function removePlayingClass(event){
+		//debugger;
+		if (event.propertyName !== "transform") {
+			return;
+		} else {
+			//remove playing class here from active div
+			console.log('transition is done!', `${event.propertName}`);
+			event.target.classList.remove('playing');
+		}
+	}
+
+	const keys = Array.from(document.querySelectorAll('.keys'));
+
+	keys.forEach(key => key.addEventListener("transitionend", removePlayingClass));
 
 	window.addEventListener("keydown", playDrumKitSound);
 })();
